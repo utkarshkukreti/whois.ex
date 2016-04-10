3f61d7765c5c59c3b8b5e3d0dac35d9c25044bce
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2016-04-10 18:02:19 +0530

    feat(Whois.Record.parse/2): parse {created,updated_expires}_at

--- lib/whois/record.ex
+++ lib/whois/record.ex
@@ -12,9 +12,41 @@ defmodule Whois.Record do
           %{record | registrar: registrar}
         "Sponsoring Registrar: " <> registrar ->
           %{record | registrar: registrar}
+        "Creation Date: " <> created_at ->
+          %{record | created_at: parse_date(created_at)}
+        "Updated Date: " <> updated_at ->
+          %{record | updated_at: parse_date(updated_at)}
+        "Expiration Date: " <> expires_at ->
+          %{record | expires_at: parse_date(expires_at)}
+        "Registry Expiry Date: " <> expires_at ->
+          %{record | expires_at: parse_date(expires_at)}
         _ ->
           record
       end
     end)
+  end
+
+  defp parse_date(string) do
+    case Regex.run(~r/^(\d{2})-([a-zA-Z]{3})-(\d{4})$/, string) do
+      [_, day, month, year] ->
+        day = String.to_integer(day)
+        month = case month do
+                  "jan" -> 1; "feb" -> 2; "mar" -> 3; "apr" -> 4
+                  "may" -> 5; "jun" -> 6; "jul" -> 7; "aug" -> 8
+                  "sep" -> 9; "oct" -> 10; "nov" -> 11; "dec" -> 12
+                end
+        year = String.to_integer(year)
+        %{day: day, month: month, year: year}
+      _ -> nil
+    end
+    ||
+    case Regex.run(~r/^(\d{4})-(\d{2})-(\d{2})T\d{2}:\d{2}:\d{2}Z$/, string) do
+      [_, year, month, day] ->
+        day = String.to_integer(day)
+        month = String.to_integer(month)
+        year = String.to_integer(year)
+        %{day: day, month: month, year: year}
+      _ -> nil
+    end
   end
 end
--- test/whois_test.exs
+++ test/whois_test.exs
@@ -12,6 +12,9 @@ defmodule WhoisTest do
                                              "NS3.GOOGLE.COM",
                                              "NS4.GOOGLE.COM"]
     assert record.registrar == "MARKMONITOR INC."
+    assert record.created_at == dmy(15, 9, 1997)
+    assert record.updated_at == dmy(20, 7, 2011)
+    assert record.expires_at == dmy(14, 9, 2020)
     assert raw =~ "Domain Name: GOOGLE.COM"
     assert raw =~ "Registrar: MARKMONITOR INC."
     assert raw =~ "Name Server: NS1.GOOGLE.COM"
@@ -26,6 +29,9 @@ defmodule WhoisTest do
                                              "NS3.GOOGLE.COM",
                                              "NS4.GOOGLE.COM"]
     assert record.registrar == "MARKMONITOR INC."
+    assert record.created_at == dmy(15, 3, 1999)
+    assert record.updated_at == dmy(12, 2, 2016)
+    assert record.expires_at == dmy(15, 3, 2017)
     assert raw =~ "Domain Name: GOOGLE.NET"
     assert raw =~ "Registrar: MARKMONITOR INC."
     assert raw =~ "Name Server: NS1.GOOGLE.COM"
@@ -40,9 +46,16 @@ defmodule WhoisTest do
                                              "NS3.GOOGLE.COM",
                                              "NS4.GOOGLE.COM"]
     assert record.registrar == "MarkMonitor Inc."
+    assert record.created_at == dmy(21, 10, 1998)
+    assert record.updated_at == dmy(18, 9, 2015)
+    assert record.expires_at == dmy(20, 10, 2016)
     assert raw =~ "Domain Name: GOOGLE.ORG"
     assert raw =~ "Sponsoring Registrar: MarkMonitor Inc."
     assert raw =~ "Name Server: NS1.GOOGLE.COM"
     assert raw =~ "Creation Date: 1998-10-21"
+  end
+
+  defp dmy(day, month, year) do
+    %{day: day, month: month, year: year}
   end
 end

8d44f0e03f8d01f0ba1351e8acb6fd4b4cc1cf60
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2016-04-10 14:27:56 +0530

    feat(Whois.lookup/1)

--- lib/whois.ex
+++ lib/whois.ex
@@ -1,2 +1,24 @@
 defmodule Whois do
+  @servers %{
+    "com" => %{host: 'whois.verisign-grs.com', prefix: "="},
+    "net" => %{host: 'whois.verisign-grs.com'},
+    "org" => %{host: 'whois.pir.org'}
+  }
+
+  def lookup(domain) do
+    [_, tld] = String.split(domain, ".", parts: 2)
+    server = Map.fetch!(@servers, tld)
+    host = server[:host]
+    prefix = server[:prefix] || ""
+    with {:ok, socket} <- :gen_tcp.connect(host, 43, [:binary, active: false]),
+         :ok <- :gen_tcp.send(socket, "#{prefix}#{domain}\r\n"),
+         do: {:ok, recv(socket)}
+  end
+
+  defp recv(socket, acc \\ "") do
+    case :gen_tcp.recv(socket, 0) do
+      {:ok, data} -> recv(socket, acc <> data)
+      {:error, :closed} -> acc
+    end
+  end
 end
--- test/whois_test.exs
+++ test/whois_test.exs
@@ -2,7 +2,27 @@ defmodule WhoisTest do
   use ExUnit.Case
   doctest Whois
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "lookup(\"google.com\")" do
+    assert {:ok, data} = Whois.lookup("google.com")
+    assert data =~ "Domain Name: GOOGLE.COM"
+    assert data =~ "Registrar: MARKMONITOR INC."
+    assert data =~ "Name Server: NS1.GOOGLE.COM"
+    assert data =~ "Creation Date: 15-sep-1997"
+  end
+
+  test "lookup(\"google.net\")" do
+    assert {:ok, data} = Whois.lookup("google.net")
+    assert data =~ "Domain Name: GOOGLE.NET"
+    assert data =~ "Registrar: MARKMONITOR INC."
+    assert data =~ "Name Server: NS1.GOOGLE.COM"
+    assert data =~ "Creation Date: 15-mar-1999"
+  end
+
+  test "lookup(\"google.org\")" do
+    assert {:ok, data} = Whois.lookup("google.org")
+    assert data =~ "Domain Name: GOOGLE.ORG"
+    assert data =~ "Sponsoring Registrar: MarkMonitor Inc."
+    assert data =~ "Name Server: NS1.GOOGLE.COM"
+    assert data =~ "Creation Date: 1998-10-21"
   end
 end

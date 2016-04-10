74c96a0ee2ee6e57b9f0612023f71d25ed0944d4
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2016-04-10 19:13:39 +0530

    refactor(WhoisTest)

--- test/whois_test.exs
+++ test/whois_test.exs
@@ -1,12 +1,10 @@
 defmodule WhoisTest do
-  alias Whois.Record
   use ExUnit.Case
   doctest Whois
 
-
   test "lookup(\"google.com\")" do
-    assert {:ok, %Record{domain: "GOOGLE.COM",
-                         raw: raw} = record} = Whois.lookup("google.com")
+    assert {:ok, record} = Whois.lookup("google.com")
+    assert record.domain == "GOOGLE.COM"
     assert Enum.sort(record.nameservers) == ["NS1.GOOGLE.COM",
                                              "NS2.GOOGLE.COM",
                                              "NS3.GOOGLE.COM",
@@ -15,15 +13,11 @@ defmodule WhoisTest do
     assert record.created_at == dmy(15, 9, 1997)
     assert record.updated_at == dmy(20, 7, 2011)
     assert record.expires_at == dmy(14, 9, 2020)
-    assert raw =~ "Domain Name: GOOGLE.COM"
-    assert raw =~ "Registrar: MARKMONITOR INC."
-    assert raw =~ "Name Server: NS1.GOOGLE.COM"
-    assert raw =~ "Creation Date: 15-sep-1997"
   end
 
   test "lookup(\"google.net\")" do
-    assert {:ok, %Record{domain: "GOOGLE.NET",
-                         raw: raw} = record} = Whois.lookup("google.net")
+    assert {:ok, record} = Whois.lookup("google.net")
+    assert record.domain == "GOOGLE.NET"
     assert Enum.sort(record.nameservers) == ["NS1.GOOGLE.COM",
                                              "NS2.GOOGLE.COM",
                                              "NS3.GOOGLE.COM",
@@ -32,15 +26,11 @@ defmodule WhoisTest do
     assert record.created_at == dmy(15, 3, 1999)
     assert record.updated_at == dmy(12, 2, 2016)
     assert record.expires_at == dmy(15, 3, 2017)
-    assert raw =~ "Domain Name: GOOGLE.NET"
-    assert raw =~ "Registrar: MARKMONITOR INC."
-    assert raw =~ "Name Server: NS1.GOOGLE.COM"
-    assert raw =~ "Creation Date: 15-mar-1999"
   end
 
   test "lookup(\"google.org\")" do
-    assert {:ok, %Record{domain: "GOOGLE.ORG",
-                         raw: raw} = record} = Whois.lookup("google.org")
+    assert {:ok, record} = Whois.lookup("google.org")
+    assert record.domain == "GOOGLE.ORG"
     assert Enum.sort(record.nameservers) == ["NS1.GOOGLE.COM",
                                              "NS2.GOOGLE.COM",
                                              "NS3.GOOGLE.COM",
@@ -49,10 +39,6 @@ defmodule WhoisTest do
     assert record.created_at == dmy(21, 10, 1998)
     assert record.updated_at == dmy(18, 9, 2015)
     assert record.expires_at == dmy(20, 10, 2016)
-    assert raw =~ "Domain Name: GOOGLE.ORG"
-    assert raw =~ "Sponsoring Registrar: MarkMonitor Inc."
-    assert raw =~ "Name Server: NS1.GOOGLE.COM"
-    assert raw =~ "Creation Date: 1998-10-21"
   end
 
   defp dmy(day, month, year) do

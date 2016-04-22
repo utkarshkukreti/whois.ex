776fd5411c31e1ceefea5a263008d8a784fa6a05
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2016-04-22 17:19:08 +0530

    feat(Whois.lookup/2): allow passing :server option

--- lib/whois.ex
+++ lib/whois.ex
@@ -1,14 +1,21 @@
 defmodule Whois do
   alias Whois.{Record, Server}
 
+  @type lookup_option :: {:server, String.t | Server.t}
+
   @doc """
   Queries the appropriate WHOIS server for the domain name `domain` and returns
   a `{:ok, %Whois.Record{}}` tuple on success, and `{:error, reason}` on
   failure.
   """
-  @spec lookup(String.t) :: {:ok, Record.t} | {:error, atom}
-  def lookup(domain) do
-    case Server.for(domain) do
+  @spec lookup(String.t, [lookup_option]) :: {:ok, Record.t} | {:error, atom}
+  def lookup(domain, opts \\ []) do
+    server = case Keyword.fetch(opts, :server) do
+               {:ok, host} when is_binary(host) -> {:ok, %Server{host: host}}
+               {:ok, %Server{} = server} -> {:ok, server}
+               :error -> Server.for(domain)
+             end
+    case server do
       {:ok, %Server{host: host, prefix: prefix}} ->
         with {:ok, socket} <- :gen_tcp.connect(String.to_char_list(host),
                                                43,
--- test/whois_test.exs
+++ test/whois_test.exs
@@ -2,7 +2,18 @@ defmodule WhoisTest do
   use ExUnit.Case
   doctest Whois
 
-  test "lookup(\"google.com\")" do
-    assert {:ok, _} = Whois.lookup("google.com")
+  test "lookup/1" do
+    assert {:ok, record} = Whois.lookup("google.com")
+    assert record.domain == "GOOGLE.COM"
+  end
+
+  test "lookup/2 with custom :server" do
+    server = "whois.markmonitor.com"
+    assert {:ok, record} = Whois.lookup("google.com", server: server)
+    assert record.domain == "google.com"
+
+    server = %Whois.Server{host: "whois.markmonitor.com"}
+    assert {:ok, record} = Whois.lookup("google.com", server: server)
+    assert record.domain == "google.com"
   end
 end

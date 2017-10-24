8ba47d9163b5b8ab988ffa1dd5efafff243caed9
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2017-10-24 21:52:46 +0530

    feat(Whois.Server): read whois hosts from `data/tld.csv` at compile time
    Fixes #1. Closes #2.

--- lib/whois/server.ex
+++ lib/whois/server.ex
@@ -1,22 +1,23 @@
 defmodule Whois.Server do
-  alias __MODULE__
-
   defstruct [:host]
 
   @type t :: %__MODULE__{host: String.t}
 
+  @all File.read!(Path.expand("../../data/tld.csv", __DIR__))
+  |> String.trim
+  |> String.split("\n")
+  |> Enum.map(fn line ->
+    [tld, host] = String.split(line, ",")
+    {tld, %{__struct__: __MODULE__, host: host}}
+  end)
+  |> Map.new
+
   @spec all :: map
-  def all do
-    %{
-      "com" => %Server{host: "whois.verisign-grs.com"},
-      "net" => %Server{host: "whois.verisign-grs.com"},
-      "org" => %Server{host: "whois.pir.org"}
-    }
-  end
+  def all, do: @all
 
   @spec for(String.t) :: {:ok, t} | :error
   def for(domain) do
     [_, tld] = String.split(domain, ".", parts: 2)
-    Map.fetch(all(), tld)
+    Map.fetch(@all, tld)
   end
 end

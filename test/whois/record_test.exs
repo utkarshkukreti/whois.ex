8546eea2edf13b2f26514870fa9a90c4e42d2821
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2017-10-24 21:25:29 +0530

    fix(test): update all fixtures and fix broken tests

--- test/whois/record_test.exs
+++ test/whois/record_test.exs
@@ -4,12 +4,12 @@ defmodule Whois.RecordTest do
 
   test "parse google.com" do
     record = parse("google.com")
-    assert record.domain == "google.com"
+    assert record.domain == "GOOGLE.COM"
     assert record.nameservers == ["ns1.google.com",
                                   "ns2.google.com",
                                   "ns3.google.com",
                                   "ns4.google.com"]
-    assert record.registrar == "MarkMonitor, Inc."
+    assert record.registrar == "MarkMonitor Inc."
     assert record.created_at == dmy(15, 9, 1997)
     assert record.updated_at == dmy(20, 7, 2011)
     assert record.expires_at == dmy(14, 9, 2020)
@@ -17,15 +17,15 @@ defmodule Whois.RecordTest do
 
   test "parse google.net" do
     record = parse("google.net")
-    assert record.domain == "google.net"
-    assert record.nameservers == ["ns1.google.com",
-                                  "ns2.google.com",
-                                  "ns3.google.com",
-                                  "ns4.google.com"]
-    assert record.registrar == "MarkMonitor, Inc."
+    assert record.domain == "GOOGLE.NET"
+    assert record.nameservers == ["ns1.google.net",
+                                  "ns2.google.net",
+                                  "ns3.google.net",
+                                  "ns4.google.net"]
+    assert record.registrar == "MarkMonitor Inc."
     assert record.created_at == dmy(15, 3, 1999)
-    assert record.updated_at == dmy(12, 2, 2016)
-    assert record.expires_at == dmy(15, 3, 2017)
+    assert record.updated_at == dmy(7, 9, 2017)
+    assert record.expires_at == dmy(15, 3, 2018)
   end
 
   test "parse google.org" do
@@ -37,8 +37,8 @@ defmodule Whois.RecordTest do
                                              "ns4.google.com"]
     assert record.registrar == "MarkMonitor Inc."
     assert record.created_at == dmy(21, 10, 1998)
-    assert record.updated_at == dmy(18, 9, 2015)
-    assert record.expires_at == dmy(20, 10, 2016)
+    assert record.updated_at == dmy(18, 9, 2017)
+    assert record.expires_at == dmy(20, 10, 2018)
   end
 
   defp parse(domain) do
--- test/whois_test.exs
+++ test/whois_test.exs
@@ -4,13 +4,16 @@ defmodule WhoisTest do
 
   test "lookup/1" do
     assert {:ok, record} = Whois.lookup("google.com")
-    assert record.domain == "google.com"
+    assert record.domain == "GOOGLE.COM"
   end
 
   test "lookup/2 with custom :server" do
     server = "whois.markmonitor.com"
     assert {:ok, record} = Whois.lookup("google.com", server: server)
     assert record.domain == "google.com"
+
+    # Wait a while before making a request to the same server again below.
+    :timer.sleep(500)
 
     server = %Whois.Server{host: "whois.markmonitor.com"}
     assert {:ok, record} = Whois.lookup("google.com", server: server)

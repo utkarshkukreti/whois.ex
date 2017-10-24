45185e0426e5302a7e402708a3b65b78563cf198
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2017-10-24 21:20:15 +0530

    feat(mix.exs): bump ex_doc to 0.18.1, remove earmark
    earmark is a dependency of ex_doc, so it'll be fetched automatically.

--- mix.exs
+++ mix.exs
@@ -33,8 +33,7 @@ defmodule Whois.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:earmark, "~> 0.1", only: :dev},
-     {:ex_doc, "~> 0.11", only: :dev}]
+    [{:ex_doc, "~> 0.18.1", only: :dev}]
   end
 
   defp package do

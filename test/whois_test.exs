a040f663d40954822d72fa5c3949945b1b9734fb
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2017-10-24 21:26:40 +0530

    feat(test): tag live tests as such and don't run them by default

--- test/whois_test.exs
+++ test/whois_test.exs
@@ -2,11 +2,13 @@ defmodule WhoisTest do
   use ExUnit.Case
   doctest Whois
 
+  @tag :live
   test "lookup/1" do
     assert {:ok, record} = Whois.lookup("google.com")
     assert record.domain == "GOOGLE.COM"
   end
 
+  @tag :live
   test "lookup/2 with custom :server" do
     server = "whois.markmonitor.com"
     assert {:ok, record} = Whois.lookup("google.com", server: server)

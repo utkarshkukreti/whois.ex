4238ea82f661d36fb167f550d3293f15c619a94c
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2017-10-26 09:13:51 +0530

    feat: bump version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Whois.Mixfile do
 
   def project do
     [app: :whois,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

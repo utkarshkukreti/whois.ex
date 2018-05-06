bf9326aff4bdd083e4ad01ac513d596774e2955f
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2018-05-06 16:38:41 +0530

    refactor(WhoisTest): assert more things, hopefully fix Travis CI run

--- test/whois_test.exs
+++ test/whois_test.exs
@@ -6,19 +6,26 @@ defmodule WhoisTest do
   test "lookup/1" do
     assert {:ok, record} = Whois.lookup("google.com")
     assert record.domain == "google.com"
+
+    for type <- [:administrator, :registrant, :technical] do
+      assert record.contacts[type].name == "Domain Administrator"
+      assert record.contacts[type].organization == "Google LLC"
+      assert record.contacts[type].city == "Mountain View"
+    end
   end
 
   @tag :live
   test "lookup/2 with custom :server" do
+    wait()
     server = "whois.markmonitor.com"
     assert {:ok, record} = Whois.lookup("google.com", server: server)
     assert record.domain == "google.com"
 
-    # Wait a while before making a request to the same server again below.
-    :timer.sleep(500)
-
+    wait()
     server = %Whois.Server{host: "whois.markmonitor.com"}
     assert {:ok, record} = Whois.lookup("google.com", server: server)
     assert record.domain == "google.com"
   end
+
+  defp wait, do: Process.sleep(2500)
 end
